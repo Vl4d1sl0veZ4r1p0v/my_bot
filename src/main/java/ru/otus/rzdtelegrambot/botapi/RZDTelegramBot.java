2c194548627bd128a44077be9238693f0d3402df
Vladislav Zaripov <vladislavzaripov.vz@gmail.com>
2020-11-08 14:38:34 +0500

    fix

---
+++ src/main/java/ru/otus/rzdtelegrambot/botapi/RZDTelegramBot.java
@@ -0,0 +1,54 @@
+package ru.otus.rzdtelegrambot.botapi;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RZDTelegramBot extends TelegramWebhookBot {
+    String botPath;
+    String botUsername;
+    String botToken;
+
+
+    public RZDTelegramBot(DefaultBotOptions options, TelegramFacade telegramFacade) {
+        super(options);
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        if (update.getMessage() != null && update.getMessage().hasText()) {
+            long chat_id = update.getMessage().getChatId();
+
+
+            try {
+                execute(new SendMessage(chat_id, "Hi " + update.getMessage().getText()));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+}
+
---
+++ src/main/java/ru/otus/rzdtelegrambot/botconfig/RZDTelegramBotConfig.java
@@ -0,0 +1,24 @@
+package ru.otus.rzdtelegrambot.botconfig;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Sergei Viacheslaev
+ */
+
+@Component
+@ConfigurationProperties(prefix = "telegrambot")
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RZDTelegramBotConfig {
+    String webHookPath;
+    String userName;
+    String botToken;
+}
+
